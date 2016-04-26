3c70708a4348b910f6fca6d8a8504cc105d78232
christineboghammar <christine@boghammar.se>
2016-04-26 15:34:21 +0200

    Init

---
+++ src/Client/Main.java
@@ -0,0 +1,7 @@
+package Client;
+
+/**
+ * Created by christineboghammar on 26/04/16.
+ */
+public class Main {
+}
---
+++ src/Server/Main.java
@@ -0,0 +1,7 @@
+package Server;
+
+/**
+ * Created by christineboghammar on 26/04/16.
+ */
+public class Main {
+}
